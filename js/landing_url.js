9578e7e3371012a5cdd5577c5123ae1c181276b6
vitrinasd <110494656+vitrinasd@users.noreply.github.com>
2022-09-21 19:06:06 +0300

    Add files via upload

---
+++ js/landing_url.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function () {
+    const addres = window.location.origin;
+    const lUrl = document.getElementById('lUrl');
+
+    lUrl.value = `${addres}`;
+ 
+})
